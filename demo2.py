3e19e8177fa0d2141eebfea642150612ba809b9e
Quentin <Quentin.vekeman@gmail.com>
2019-12-19 02:57:00 +0100

    few more tweaks for presentation

--- demo2.py
+++ demo2.py
@@ -6,5 +6,5 @@ data.deskew_data()
 svm.train_svm('rbf', 4, data.x_train, data.y_train, data.x_val, data.y_val)
 
 # Some more test data predictions
-svm.predict_number(data.x_test, 0)
-svm.predict_number(data.x_test, 2)
+# svm.predict_number(data.x_test, 0)
+# svm.predict_number(data.x_test, 2)
--- demo3.py
+++ demo3.py
@@ -2,6 +2,7 @@ import data
 import cnn
 
 # Classic NN with 2 layers before deskewing
+print("Classic NN with 2 layers before deskewing")
 cnn.train_nn(data.x_train,
              data.y_train,
              data.x_val,
@@ -12,6 +13,7 @@ cnn.train_nn(data.x_train,
              verbosity=0)
 
 # Classic NN with 2 layers after deskewing
+print("Classic NN with 2 layers after deskewing")
 data.deskew_data()
 
 cnn.train_nn(data.x_train,
@@ -24,6 +26,7 @@ cnn.train_nn(data.x_train,
              verbosity=0)
 
 # Complex CNN
+print("Complex CNN")
 cnn.train_cnn(data.x_train, data.y_train, data.x_val, data.y_val, batch_size=86, epochs=2)
 
-# Next: Further tweak cnn / design my own
+# Next: Further tweak cnn / design my own and export the data
