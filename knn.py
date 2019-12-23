3eb782446b7754e3a45b22b3c8eef3ad518120ea
Herblerp <Quentin.vekeman@gmail.com>
2019-12-23 20:49:03 +0100

    Add notebook for tests

--- knn.py
+++ knn.py
@@ -9,7 +9,6 @@ def train_knn(neighbours, x_train, y_train, x_val, y_val):
     knn = KNeighborsClassifier(n_neighbors=neighbours, n_jobs=-1)
     knn.fit(x_train, y_train)
     knn_accuracy = round(knn.score(x_val, y_val) * 100, 2)
-    print("Accuracy for %d neighbours = %f" % (neighbours, knn_accuracy))
 
     return knn_accuracy
 
---
+++ test.py
@@ -0,0 +1,71 @@
+import data
+import knn
+import svm
+import cnn
+
+import plotly.graph_objs as go
+import plotly.offline as plotly
+
+data.shuffle_data()
+data.deskew_data()
+
+
+def test_knn_neighbours(k_max, iterations):
+    accuracies = []
+    number_of_neighbors = []
+    for k in range(1, k_max+1):
+        iteration_accuracies = []
+        for iteration in range(1, iterations+1):
+            data.shuffle_data()
+            knn_accuracy = knn.train_knn(k, data.x_train, data.y_train, data.x_val, data.y_val)
+            iteration_accuracies.append(round(knn_accuracy, 2))
+            print('Accuracy for iteration %d with %d neighbours is %f' % (iteration, k, knn_accuracy))
+
+        average_accuracy = sum(iteration_accuracies) / len(iteration_accuracies)
+        print('Average accuracy for %d neighbours is %f' % (k, average_accuracy))
+        accuracies.append(average_accuracy)
+        number_of_neighbors.append(k)
+
+    trace1 = go.Scatter(
+        y=accuracies,
+        x=number_of_neighbors,
+        mode="lines",
+        name="K-NN Classifier",
+    )
+
+    graph_data = [trace1]
+    layout = dict(title='KNN Accuracy',
+                  autosize=False,
+                  width=800,
+                  height=500,
+                  yaxis=dict(title='Validation Accuracy (%)', gridwidth=2, gridcolor='#bdbdbd'),
+                  xaxis=dict(title='Number of Neighbors', gridwidth=2, gridcolor='#bdbdbd'),
+                  font=dict(size=14)
+                  )
+    fig = dict(data=graph_data, layout=layout)
+    plotly.iplot(fig)
+
+
+def knntest():
+    knn_iterations = 2
+    knn_accuracies = []
+
+    for x in range(knn_iterations):
+        data.shuffle_data()
+        knn_accuracies.append(knn.train_knn(3, data.x_train, data.y_train, data.x_val, data.y_val))
+
+    knn_average = sum(knn_accuracies) / len(knn_accuracies)
+    print("Average of knn for %d iterations is %f" % (knn_iterations, round(knn_average, 2)))
+
+
+    svm_iterations = 2
+    svm_accuracies = []
+
+    for x in range(svm_iterations):
+        data.shuffle_data()
+        svm_accuracies.append(svm.train_svm('rbf', 0, data.x_train, data.y_train, data.x_val, data.y_val))
+
+    svm_average = sum(svm_accuracies) / len(svm_accuracies)
+    print("Average of svm for %d iterations is %f" % (svm_iterations, round(svm_average, 2)))
+
+
